7409cc8b3d0096a09e4b749f49102562808b3b12
RMacW98 <c17407712@mydit.ie>
2020-12-02 14:52:58 +0000

    Updated some css and added an app icon

--- DjangoApp/DjangoApp/settings.py
+++ DjangoApp/DjangoApp/settings.py
@@ -211,11 +211,11 @@ PWA_APP_START_URL = '/'
 PWA_APP_STATUS_BAR_COLOR = 'default'
 PWA_APP_ICONS = [
     {
-        'src': '/static/images/globe_icon_192px.png',
+        'src': '/static/images/cryptoicon.png',
         'sizes': '192x192'
     },
     {
-        'src': '/static/images/globe_icon_512px.png',
+        'src': '/static/images/cryptoicon.png',
         'sizes': '512x512'
     }
 ]
